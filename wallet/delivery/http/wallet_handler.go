8724cb656f8d968a739e6476a98b3d0aa2a474b9
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-06 01:09:57 +0600

    init: create skeleton for wallet handler

--- wallet/delivery/http/wallet_handler.go
+++ wallet/delivery/http/wallet_handler.go
@@ -1,1 +1,33 @@
 package http
+
+import (
+	"github.com/buaazp/fasthttprouter"
+	"github.com/dabarov/bank-transaction-service/domain"
+	"github.com/valyala/fasthttp"
+)
+
+type WalletHandler struct {
+	WUsecase domain.WalletUsecase
+}
+
+func NewWalletHandler(router *fasthttprouter.Router, wcase domain.WalletUsecase) {
+	handler := &WalletHandler{
+		WUsecase: wcase,
+	}
+	router.POST("/wallet/create", handler.Create)
+	router.PUT("/wallet/deposit", handler.Deposit)
+	router.PUT("/wallet/transfer", handler.Transfer)
+	router.GET("/wallet/", handler.GetUserWallets)
+}
+
+func (w *WalletHandler) Create(ctx *fasthttp.RequestCtx) {
+}
+
+func (w *WalletHandler) Deposit(ctx *fasthttp.RequestCtx) {
+}
+
+func (w *WalletHandler) Transfer(ctx *fasthttp.RequestCtx) {
+}
+
+func (w *WalletHandler) GetUserWallets(ctx *fasthttp.RequestCtx) {
+}
