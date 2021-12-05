ddcf6ecfd763ad818f226bb54632d5b34141c119
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-06 02:42:05 +0600

    fix: remove user domain

--- domain/user.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type User struct {
-	IIN       string `json:"iin"`
-	Login     string `json:"login"`
-	Password  string `json:"password"`
-	CreatedAt string `json:"created_at"`
-}
--- domain/wallet.go
+++ domain/wallet.go
@@ -4,7 +4,7 @@ import "context"
 
 type Wallet struct {
 	ID                  uint64 `json:"id"`
-	User                User   `json:"user"`
+	UserIIN             string `json:"user"`
 	LastTransactionDate string `json:"last_transaction_date"`
 	Balance             string `json:"balance"`
 	CreatedAt           string `json:"created_at"`
