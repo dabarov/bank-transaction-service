6b1f7976645585324ff02aaba5dc65cf201939df
Aldiyar Dabarov <simlyalde@gmail.com>
2021-12-06 02:47:21 +0600

    feat: add create function for postgres

--- wallet/repository/postgresql/postgresql_wallet.go
+++ wallet/repository/postgresql/postgresql_wallet.go
@@ -2,6 +2,8 @@ package postgresql
 
 import (
 	"context"
+	"math/rand"
+	"time"
 
 	"github.com/dabarov/bank-transaction-service/domain"
 	"gorm.io/gorm"
@@ -16,7 +18,17 @@ func NewWalletPostgresqlRepository(Conn *gorm.DB) domain.WalletRepository {
 }
 
 func (w *walletPostgresqlRepository) Create(ctx context.Context, iin string) error {
+	wallet := &domain.Wallet{
+		ID:                  rand.Uint64(),
+		UserIIN:             iin,
+		LastTransactionDate: time.Time{}.String(),
+		Balance:             0,
+		CreatedAt:           time.Now().String(),
+	}
 
+	if err := w.Conn.Create(&wallet).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
